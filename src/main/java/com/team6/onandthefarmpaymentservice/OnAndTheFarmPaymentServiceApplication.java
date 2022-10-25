b493b4752a3a72e0209fc067403fa474d6658671
ksh940925 <ksh9409255@naver.com>
2022-10-25 20:27:45 +0900

    init: gitignore

---
+++ src/main/java/com/team6/onandthefarmpaymentservice/OnAndTheFarmPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmpaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnAndTheFarmPaymentServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnAndTheFarmPaymentServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/onandthefarmpaymentservice/OnAndTheFarmPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmpaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnAndTheFarmPaymentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
