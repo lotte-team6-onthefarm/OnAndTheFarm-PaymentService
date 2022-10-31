37a6275d7289b5ce3aa3abf2607e89e0cc41b39c
ksh940925 <ksh9409255@naver.com>
2022-10-31 12:19:50 +0900

    fix: TCC 부분 에러 수정 -> Exception 처리를 CommonErrorHandler에서 처리하게 해야 함

--- src/main/java/com/team6/onandthefarmpaymentservice/kafka/PaymentOrderChannelAdapter.java
+++ src/main/java/com/team6/onandthefarmpaymentservice/kafka/PaymentOrderChannelAdapter.java
@@ -2,8 +2,10 @@ package com.team6.onandthefarmpaymentservice.kafka;
 
 import org.springframework.kafka.support.Acknowledgment;
 
+import java.io.IOException;
+
 public interface PaymentOrderChannelAdapter {
     void producer(String message);
 
-    void consumer(String message, Acknowledgment ack);
+    void consumer(String message, Acknowledgment ack) throws Exception;
 }
--- src/main/java/com/team6/onandthefarmpaymentservice/kafka/PaymentOrderChannelAdapterKafkaImpl.java
+++ src/main/java/com/team6/onandthefarmpaymentservice/kafka/PaymentOrderChannelAdapterKafkaImpl.java
@@ -9,11 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Service
-@Transactional
+import java.io.IOException;
+
+@Component
 @RequiredArgsConstructor
 @Slf4j
 public class PaymentOrderChannelAdapterKafkaImpl implements PaymentOrderChannelAdapter{
@@ -27,21 +29,19 @@ public class PaymentOrderChannelAdapterKafkaImpl implements PaymentOrderChannelA
         this.kafkaTemplate.send(TOPIC, message);
     }
 
-    @KafkaListener(topics = TOPIC)
-    public void consumer(String message, Acknowledgment ack) {
+    @KafkaListener(topics = TOPIC,containerFactory = "kafkaListenerContainerFactory")
+    public void consumer(String message, Acknowledgment ack) throws Exception {
         log.info(String.format("Message Received : %s", message));
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            PaymentDto paymentDto = objectMapper.readValue(message, PaymentDto.class);
-            if(paymentService.isAlreadyProcessedOrderId(paymentDto.getOrderSerial())){
-                // 중복되지 않은 메시지임으로 결제 생성
-                paymentService.createPayment(paymentDto);
-            }
-
-            // Kafka Offset Manual Commit(수동커밋)
-            ack.acknowledge();
-        } catch (Exception e) {
-            e.printStackTrace();
+        ObjectMapper objectMapper = new ObjectMapper();
+        PaymentDto paymentDto = objectMapper.readValue(message, PaymentDto.class);
+        if(paymentService.isAlreadyProcessedOrderId(paymentDto.getOrderSerial())){
+            // 중복되지 않은 메시지임으로 결제 생성
+            paymentService.createPayment(paymentDto);
         }
+
+        Long test = Long.valueOf("adsasd");
+
+        // Kafka Offset Manual Commit(수동커밋)
+        ack.acknowledge();
     }
 }
